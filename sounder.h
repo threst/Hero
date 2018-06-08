d9ac851e73600c81916295f3bf8ceb7d2f915adb
nu11hex <nu11hex.github.io>
2018-06-08 21:17:41 +0800

    Mod errors of sounder.h

--- sounder.h
+++ sounder.h
@@ -20,13 +20,13 @@ class Sounder {
 void Sounder::sounder(int f)
 {
 	mciSendString("open x.mp3 alias xxx", NULL, 0, NULL);	//更改 x.mp3 为实际路径文件名，更改 xxx 为后续调用别名
-	if (f = 1) {
+	if (f == 1) {
 		mciSendString("play xxx", NULL, 0, NULL);	//若参数为 1，则播放 xxx
 	}
-	else if (f = 2) {
-		mciSendString("stop xxx", NULL, 0, NULL);	//若参数为 2，则暂停 xxx
+	else if (f == 2) {
+		mciSendString("stop xxx", NULL, 0, NULL);	//若参数为 2，则停止 xxx
 	}
-	else if (f = 0) {
+	else if (f == 0) {
 		mciSendString("close xxx", NULL, 0, NULL);	//若参数为 0，则关闭 xxx 。关闭后若想再次打开文件，请再次调用函数
 	}
 }
